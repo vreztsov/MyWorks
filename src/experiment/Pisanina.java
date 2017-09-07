355d87aac069e0f55b4b00c607070421771cf08c
vreztsov <vreztsov90@mail.ru>
2017-09-07 21:53:28 +0300

    'message'

---
+++ src/experiment/Pisanina.java
@@ -0,0 +1,13 @@
+package experiment;
+
+/**
+ * Created by Viktor on 07.09.2017.
+ */
+public class Pisanina {
+    public static void main(String[] args) {
+        System.out.println("dfgshsrh435tqdfgb");
+        System.out.println("dfgdfnhdmtsryujuk");
+        System.out.println("gkkthodnohs");
+
+    }
+}
